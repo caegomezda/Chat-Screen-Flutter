28389091055ade6f4c5463f645f9f1a7f67f5cb8
caegomezda <camiloedg23@gmail.com>
2026-01-22 01:26:53 -0500

    Feat: Add chat message bubble widget

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -17,6 +17,31 @@ class ChatScreen extends StatelessWidget {
         title: Text("Mi amor <3"),
         centerTitle: false,
       ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric( horizontal: 10.0),
+        child: Column(
+          children: [
+            Expanded(child: 
+              ListView.builder( itemCount: 100, itemBuilder: (context, index) {
+                
+                return Text("Indice: $index");
+
+              },)
+            ),
+            Text("Hola Mundo"),
+          ],
+        ),
+      ),
     );
   }
 }
